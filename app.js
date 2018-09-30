bbfe7e7d94cd9d500a98178b3e02c1210523e49b
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2018-09-30 14:36:27 +0530

    error handling for not found routes
    error handling for not found routes

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express'),
-      app     = express();
+       morgan = require('morgan'),
+       app    = express();
 
 const productRoutes = require('./api/routes/products'),
       orderRoutes   = require('./api/routes/orders');
 
+app.use(morgan('dev'));    
+
+// Handle products and orders routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 401;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 // middleware  
 // app.use((req, res, next) => {
